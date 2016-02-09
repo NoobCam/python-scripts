7d584c1e1cfc815fbdfc013a7713b1a5c9e019a7
Cameron <kisner.cameron@gmail.com>
2016-02-09 08:53:41 -0600

    removed some files

--- anon_ftp.py
+++ anon_ftp.py
@@ -3,21 +3,21 @@
 import ftplib
 import argparse
 
-def main(hostname):
-    parser = argparse.ArgumentParser(description="Check FTP sites for ANON login.")
-    parser.add_argument('-f', '--file', type=file)
-    args = parser.parse_args()
+parser = argparse.ArgumentParser(description="Check FTP sites for ANON login.")
+parser.add_argument('-f', '--file', type=file)
+args = parser.parse_args()
 
-	for word in args.file.read().split():
-            ftp = ftplib.FTP(word)
+
+def ftplogin(hostname):
+    for word in args.file.read().split():    
+        try: 
+            ftp = ftplib.FTP(word,timeout=3)
             ftp.login('anonymous', 'none@sun.com')
             print '\n[*] ' + str(word) + ' FTP Anonymous Logon Succeeded.'
             ftp.quit()
-            return True
-        
-        except Exception, e:
-            print '\n[-] ' + str(word) + ' FTP Anymous Logon Failed.'
-            return False
-main(host)
+            ftp.close()
+        except Exception, ftplib.all_errors:
+            print '\n[-] ' + str(word) + ' FTP Anonymous Logon Failed.'         
 
+ftplogin(args.file)
 
